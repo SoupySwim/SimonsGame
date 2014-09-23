47fe4c1189e0d9cdb4dd5531da144fe52f6c6a03
SoupySwim <weisse.simon@gmail.com>
2014-09-23 18:44:18 -0500

    PhysicsObject
    Added Attributes and functions to PhysicsObject

--- SimonsGame/GuiObjects/BaseClasses/PhysicsObject.cs
+++ SimonsGame/GuiObjects/BaseClasses/PhysicsObject.cs
@@ -1,11 +1,29 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using SimonsGame.Modifiers;
+using SimonsGame.Utility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace SimonsGame.GuiObjects.BaseClasses
 {
-	class PhysicsObject
+	class PhysicsObject : MainGuiObject
 	{
+
+		public AbilityManager AbilityManager { get; set; }
+		public bool IsLanded { get; set; }
+		protected bool StopGravity { get; set; }
+		public Vector2 BasePosition { get; set; }
+
+		public PhysicsObject(Vector2 position, Vector2 hitbox, Group group, Level level)
+			: base(position, hitbox, group, level)
+		{
+		}
+		public abstract void PostPhysicsPreUpdate(GameTime gameTime) { }
+		public override void PreUpdate(GameTime gameTime) { }
+		public override void AddCustomModifiers(GameTime gameTime, ModifierBase modifyAdd) { }
+		public override void MultiplyCustomModifiers(GameTime gameTime, ModifierBase modifyMult) { }
+		public void ForceAbility(GameTime gameTime) { }
 	}
 }
---
+++ SimonsGame/Modifiers/Abilities/AbilityManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimonsGame.Modifiers
+{
+	// To be implemented later.
+
+	class AbilityManager
+	{
+	}
+}
